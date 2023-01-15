75db8f0b67dcc0e7d9ba0a3672be288322acdc55
trugal <122724015+trugal@users.noreply.github.com>
2023-01-15 13:09:12 +0300

    Add files via upload

---
+++ whoisitemployee/main.py
@@ -0,0 +1,31 @@
+
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+menu = ['Установка', 'Первое приложение', 'Обратная связь']
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    print(url_for('index'))
+    return render_template('index.html', menu=menu)
+
+
+@app.route("/about")
+def about():
+    print(url_for('about'))
+    return render_template('about.html', title='О сайте', menu=menu)
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    return f"Пользователь: {username}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# with app.test_request_context():
+#    print(url_for('about'))
